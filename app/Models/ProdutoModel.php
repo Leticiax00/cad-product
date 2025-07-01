89fecb945f2854c04747a71a8f3152a783f2a605
Leticiax00 <marialeticiaramosb@gmail.com>
2025-07-01 14:01:16 -0300

    salvo

---
+++ app/Models/ProdutoModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ProdutoModel extends Model
+{
+    protected $table = 'produtos';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['nome', 'preco', 'quantidade'];
+}
