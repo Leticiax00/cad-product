85cc9d9e4a902de7badf504d1ed7e0e01fc73c56
Letícia Ramos <marialeticiaramosb@gmail.com>
2025-07-03 11:26:45 -0300

    banco de dados

---
+++ app/Config/Routes.php
@@ -0,0 +1,26 @@
+<?php
+//  rotas do index para os metodos 
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->post('/Produtos/salvar', 'Produtos::salvar'); // caminho dos controllers para as funções dos controllers 
+
+// rota post para /produtos/salvar, direcionando para o metodo salvar do controlador - produtos.php (envio de formularios)
+
+$routes->get('/Produtos/sucesso', 'Produtos::sucesso'); // puxa a função sucesso do controlador (produtos) para a mensagem de sucesso para produto cadastrado
+
+$routes->post('/produto_excluir/(:num)', 'Home::excluirProduto/$1'); //id do produto(:num)// método excluir produto, passando o valor para o argumento
+#'Home::excluirProduto/$1'); 
+
+#$routes->get('/produto/editar/', 'Home::editar/$1'));
+
+$routes->get('/Produto/editar/(:num)', 'Home::editar/$1');
+// chama o método editar para editar um produto pelo ID.
+
+$routes->get('/form', function () {
+    return view('form'); // rota para retornar a view
+});
